cee6f81539b0143d2dd113a3aa0a863fa5847652
Bogdan Cochior <bogdan.cochior@thinslices.com>
2017-11-24 11:12:31 +0200

    Add redux-logger

--- src/Config/store.config.js
+++ src/Config/store.config.js
@@ -1,13 +1,20 @@
+import logger from 'redux-logger'
 import createSagaMiddleWare from 'redux-saga'
 import { applyMiddleware, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 export default (rootReducer, rootSaga, history, startUpFn) => {
+  const middleWares = []
+
   const sagaMiddleWare = createSagaMiddleWare()
+  middleWares.push(sagaMiddleWare)
+
+  middleWares.push(logger)
+
   const store = createStore(
     rootReducer,
     {},
-    composeWithDevTools(applyMiddleware(sagaMiddleWare))
+    composeWithDevTools(applyMiddleware(...middleWares))
   )
   
   sagaMiddleWare.run(rootSaga)
